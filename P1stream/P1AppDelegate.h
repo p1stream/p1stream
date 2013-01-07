96e0e2710f2239633e5c88b3da419baec4b69f35
Stéphan Kochen <stephan@kochen.nl>
2013-01-07 23:37:57 +0100

    Initial Commit

---
+++ P1stream/P1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  P1AppDelegate.h
+//  P1stream
+//
+//  Created by Stéphan Kochen on 2013-01-07.
+//  Copyright (c) 2013 Stéphan Kochen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface P1AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ P1stream/P1AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  P1AppDelegate.m
+//  P1stream
+//
+//  Created by Stéphan Kochen on 2013-01-07.
+//  Copyright (c) 2013 Stéphan Kochen. All rights reserved.
+//
+
+#import "P1AppDelegate.h"
+
+@implementation P1AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
