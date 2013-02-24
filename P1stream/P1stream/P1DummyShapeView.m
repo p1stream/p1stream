c6c51c664ea3377e1e1783a4f2c1155a9d461b9c
Stéphan Kochen <stephan@kochen.nl>
2013-02-24 14:57:28 +0100

    Minor shape view tweak: clip to dirtyRect.

--- P1stream/P1stream/P1DummyShapeView.m
+++ P1stream/P1stream/P1DummyShapeView.m
@@ -5,13 +5,18 @@
 
 - (void)drawRect:(NSRect)dirtyRect
 {
+    [[NSGraphicsContext currentContext] saveGraphicsState];
+
     [[NSColor clearColor] set];
     NSRectFill(dirtyRect);
 
     // Slighty larger corner radius than P1Preview, to prevent color bleeding.
     [[NSColor whiteColor] set];
+    NSRectClip(dirtyRect);
     NSBezierPath *path = [NSBezierPath bezierPathWithRoundedRect:self.bounds xRadius:7 yRadius:7];
     [path fill];
+
+    [[NSGraphicsContext currentContext] restoreGraphicsState];
 }
 
 @end
