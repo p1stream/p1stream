98713a75ebb23106faf251cdbefa1f6d5df2666e
Stéphan Kochen <stephan@kochen.nl>
2014-05-24 19:40:31 +0200

    Setup module structure.

--- P1stream/osx/_third_party_main.js
+++ P1stream/osx/_third_party_main.js
@@ -1,1 +1,5 @@
-console.log("Hello world!");
+var path = require('path');
+var Module = require('module');
+
+var mainPath = path.resolve(process.execPath, '../../Resources/lib/index.js');
+Module._load(mainPath, null, true);
