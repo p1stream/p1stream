dcf948491acba1f96d00742c012bd5047d05fe1c
Stéphan Kochen <stephan@kochen.nl>
2014-06-06 11:52:11 +0200

    Improve build script, run option.

--- build.sh
+++ build.sh
@@ -1,41 +1,63 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = "-h" ]; then
-    echo "Usage: $0 [ -h | -f | -c ]"
-    echo " -h: display usage"
-    echo " -f: regenerate build.ninja and build"
-    echo " -c: clean output and build from scratch"
+CLEAN=none
+BUILD=1
+RUN=0
+
+while getopts "hbcCr" opt; do case $opt in
+h)
+    echo "Usage: $0 [-hbcCr]"
+    echo " -h: print usage then exit"
+    echo " -b: regenerate build.ninja before build"
+    echo " -c: clean output before build"
+    echo " -C: clean output then exit"
+    echo " -r: run after build"
     exit 64
-fi
+    ;;
+b) CLEAN=build ;;
+c) CLEAN=all ;;
+C) CLEAN=all; BUILD=0 ;;
+r) RUN=1 ;;
+esac done
 
 cd "$(dirname $0)"
 
-if [ "$1" = "-c" ]; then
-    echo "  CLEAN out"
-    rm -fr out/
-elif [ "$1" = "-f" ]; then
+if [ "$CLEAN" == "build" ]; then
     echo "  CLEAN out/build.ninja"
     rm -f out/build.ninja
 fi
 
-if [ ! -x "out/ninja" ]; then
-    echo "  BUILD out/ninja"
-
-    mkdir -p out/scratch/
-    git clone --shared deps/ninja out/scratch/ninja
-
-    cd out/scratch/ninja/
-    python bootstrap.py
-    cd ../../../
-
-    mv out/scratch/ninja/ninja out/ninja
-    rm -fr out/scratch/ninja
+if [ "$CLEAN" == "all" ]; then
+    echo "  CLEAN out/"
+    rm -fr out/
 fi
 
-if [ ! -f "out/build.ninja" ]; then
-    echo "  BUILD out/build.ninja"
-    tools/gen.py > out/build.ninja
-fi
+if [ "$BUILD" == "1" ]; then
 
-exec out/ninja -f out/build.ninja
+    if [ ! -x "out/ninja" ]; then
+        echo "  BUILD out/ninja"
+
+        mkdir -p out/scratch/
+        git clone --shared deps/ninja out/scratch/ninja
+
+        cd out/scratch/ninja/
+        python bootstrap.py
+        cd ../../../
+
+        mv out/scratch/ninja/ninja out/ninja
+        rm -fr out/scratch/ninja
+    fi
+
+    if [ ! -f "out/build.ninja" ]; then
+        echo "  BUILD out/build.ninja"
+        tools/gen.py > out/build.ninja
+    fi
+
+    out/ninja -f out/build.ninja
+
+    if [ "$RUN" == "1" ]; then
+        out/P1stream.app/Contents/MacOS/node
+    fi
+
+fi
