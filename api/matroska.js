74fe9faf433c738f92247e1b2cb4ba322aa1bfaa
Stéphan Kochen <stephan@kochen.nl>
2014-07-11 00:36:15 +0200

    Rewrite EBML code.
    Lift more work in C++, more concise JS.

--- api/matroska.js
+++ api/matroska.js
@@ -1,161 +1,108 @@
 var native = require('./api.node');
 
 
-function id(v) {
+function tag(id, type) {
     var b = new Buffer(4);
-    b.writeUInt32BE(v, 0);
-    if (v <= 0xFF)
-        return b.slice(3, 4);
-    else if (v <= 0xFFFF)
-        return b.slice(2, 4);
-    else if (v <= 0xFFFFFF)
-        return b.slice(1, 4);
-    else if (v <= 0xFFFFFFFF)
-        return b.slice(0, 4);
-    else
+    b.writeUInt32BE(id, 0);
+    if (id <= 0xFF)
+        b = b.slice(3, 4);
+    else if (id <= 0xFFFF)
+        b = b.slice(2, 4);
+    else if (id <= 0xFFFFFF)
+        b = b.slice(1, 4);
+    else if (id > 0xFFFFFFFF)
         throw new Error('Invalid ID');
+
+    return function(val, sizeUnknown) {
+        return [b, type, val, sizeUnknown];
+    };
 }
 
-var ID_EBML = id(0x1A45DFA3);
-var ID_EBMLVersion = id(0x4286);
-var ID_EBMLReadVersion = id(0x42F7);
-var ID_EBMLMaxIDLength = id(0x42F2);
-var ID_EBMLMaxSizeLength = id(0x42F3);
-var ID_DocType = id(0x4282);
-var ID_DocTypeVersion = id(0x4287);
-var ID_DocTypeReadVersion = id(0x4285);
-var ID_Segment = id(0x18538067);
-var ID_Info = id(0x1549A966);
-var ID_TimecodeScale = id(0x2AD7B1);
-var ID_MuxingApp = id(0x4D80);
-var ID_WritingApp = id(0x5741);
-var ID_Tracks = id(0x1654AE6B);
-var ID_TrackEntry = id(0xAE);
-var ID_TrackNumber = id(0xD7);
-var ID_TrackUID = id(0x73C5);
-var ID_TrackType = id(0x83);
-var ID_FlagEnabled = id(0xB9);
-var ID_FlagDefault = id(0x88);
-var ID_FlagForced = id(0x55AA);
-var ID_FlagLacing = id(0x9C);
-var ID_MinCache = id(0x6DE7);
-var ID_MaxBlockAdditionID = id(0x55EE);
-var ID_CodecID = id(0x86);
-var ID_CodecPrivate = id(0x63A2);
-var ID_CodecDecodeAll = id(0xAA);
-var ID_SeekPreRoll = id(0x56BB);
-var ID_Video = id(0xE0);
-var ID_FlagInterlaced = id(0x9A);
-var ID_PixelWidth = id(0xB0);
-var ID_PixelHeight = id(0xBA);
-var ID_Cluster = id(0x1F43B675);
-var ID_Timecode = id(0xE7);
-var ID_SimpleBlock = id(0xA3);
+var T = {
+    EBML: tag(0x1A45DFA3, 'm'),
+    EBMLVersion: tag(0x4286, 'u'),
+    EBMLReadVersion: tag(0x42F7, 'u'),
+    EBMLMaxIDLength: tag(0x42F2, 'u'),
+    EBMLMaxSizeLength: tag(0x42F3, 'u'),
+    DocType: tag(0x4282, 's'),
+    DocTypeVersion: tag(0x4287, 'u'),
+    DocTypeReadVersion: tag(0x4285, 'u'),
+    Segment: tag(0x18538067, 'm'),
+    Info: tag(0x1549A966, 'm'),
+    TimecodeScale: tag(0x2AD7B1, 'u'),
+    MuxingApp: tag(0x4D80, '8'),
+    WritingApp: tag(0x5741, '8'),
+    Tracks: tag(0x1654AE6B, 'm'),
+    TrackEntry: tag(0xAE, 'm'),
+    TrackNumber: tag(0xD7, 'u'),
+    TrackUID: tag(0x73C5, 'u'),
+    TrackType: tag(0x83, 'u'),
+    FlagEnabled: tag(0xB9, 'u'),
+    FlagDefault: tag(0x88, 'u'),
+    FlagForced: tag(0x55AA, 'u'),
+    FlagLacing: tag(0x9C, 'u'),
+    MinCache: tag(0x6DE7, 'u'),
+    MaxBlockAdditionID: tag(0x55EE, 'u'),
+    CodecID: tag(0x86, 's'),
+    CodecPrivate: tag(0x63A2, 'b'),
+    CodecDecodeAll: tag(0xAA, 'u'),
+    SeekPreRoll: tag(0x56BB, 'u'),
+    Video: tag(0xE0, 'm'),
+    FlagInterlaced: tag(0x9A, 'u'),
+    PixelWidth: tag(0xB0, 'u'),
+    PixelHeight: tag(0xBA, 'u'),
+    Cluster: tag(0x1F43B675, 'm'),
+    Timecode: tag(0xE7, 'u'),
+    SimpleBlock: tag(0xA3, 'b')
+};
 
-
-var varInt = native.varInt;
-
-function writeElementHead(id, size, arr) {
-    var b = varInt(size, 8);
-    arr.push(id);
-    arr.push(b);
-}
-
-function writeBinary(id, buf, arr) {
-    writeElementHead(id, buf.length, arr);
-    arr.push(buf);
-}
-
-function writeUInt8(id, num, arr) {
-    var buf = new Buffer(1);
-    buf.writeUInt8(num, 0);
-    writeBinary(id, buf, arr);
-}
-
-function writeUInt16(id, num, arr) {
-    var buf = new Buffer(2);
-    buf.writeUInt16BE(num, 0);
-    writeBinary(id, buf, arr);
-}
-
-function writeUInt32(id, num, arr) {
-    var buf = new Buffer(4);
-    buf.writeUInt32BE(num, 0);
-    writeBinary(id, buf, arr);
-}
-
-function writeUInt64(id, num, arr) {
-    var buf = new Buffer(8);
-    buf.writeUInt32BE(~~(num / 0x0000000100000000), 0);
-    buf.writeUInt32BE(num % 0x00000000FFFFFFFF, 4);
-    writeBinary(id, buf, arr);
-}
-
-function writeString(id, val, arr) {
-    var buf = new Buffer(val);
-    writeBinary(id, buf, arr);
-}
-
-function writeContainer(id, children, arr) {
-    var buf = Buffer.concat(children);
-    writeBinary(id, buf, arr);
-}
 
 
 exports.headers = function(frame) {
-    var list = [];
-    var b, l, ll, lll;
-
-    l = []; // EBML
-    writeUInt8(ID_EBMLVersion, 1, l);
-    writeUInt8(ID_EBMLReadVersion, 1, l);
-    writeUInt8(ID_EBMLMaxIDLength, 4, l);
-    writeUInt8(ID_EBMLMaxSizeLength, 8, l);
-    writeString(ID_DocType, 'matroska', l);
-    writeUInt8(ID_DocTypeVersion, 1, l);
-    writeUInt8(ID_DocTypeReadVersion, 1, l);
-    writeContainer(ID_EBML, l, list);
-
-    writeElementHead(ID_Segment, -1, list);
-
-    l = []; // Info
-    writeUInt32(ID_TimecodeScale, 1, l);
-    writeString(ID_MuxingApp, 'P1stream', l);
-    writeString(ID_WritingApp, 'P1stream', l);
-    writeContainer(ID_Info, l, list);
-
-    l = []; // Tracks
-
-    ll = []; // TrackEntry
-    writeUInt8(ID_TrackNumber, 1, ll);
-    writeUInt8(ID_TrackUID, 1, ll);
-    writeUInt8(ID_TrackType, 0x1, ll);
-    writeUInt8(ID_FlagEnabled, 1, ll);
-    writeUInt8(ID_FlagDefault, 1, ll);
-    writeUInt8(ID_FlagForced, 1, ll);
-    writeUInt8(ID_FlagLacing, 0, ll);
-    writeUInt8(ID_MinCache, 0, ll);
-    writeUInt8(ID_MaxBlockAdditionID, 0, ll);
-    writeString(ID_CodecID, 'V_MPEG4/ISO/AVC', ll);
-    writeBinary(ID_CodecPrivate, codecPrivate(frame), ll);
-    writeUInt8(ID_CodecDecodeAll, 1, ll);
-    writeUInt8(ID_SeekPreRoll, 0, ll);
-
-    lll = []; // Video
-    writeUInt8(ID_FlagInterlaced, 0, lll);
-    writeUInt16(ID_PixelWidth, 1280, lll);
-    writeUInt16(ID_PixelHeight, 720, lll);
-    writeContainer(ID_Video, lll, ll);
-
-    writeContainer(ID_TrackEntry, ll, l);
-
-    writeContainer(ID_Tracks, l, list);
-
-    return Buffer.concat(list);
+    return native.buildEBML([
+        T.EBML([
+            T.EBMLVersion(1),
+            T.EBMLReadVersion(1),
+            T.EBMLMaxIDLength(4),
+            T.EBMLMaxSizeLength(8),
+            T.DocType('matroska'),
+            T.DocTypeVersion(1),
+            T.DocTypeReadVersion(1)
+        ]),
+        T.Segment([
+            T.Info([
+                T.TimecodeScale(1),
+                T.MuxingApp('P1stream'),
+                T.WritingApp('P1stream')
+            ]),
+            T.Tracks([
+                T.TrackEntry([
+                    T.TrackNumber(1),
+                    T.TrackUID(1),
+                    T.TrackType(0x1),
+                    T.FlagEnabled(1),
+                    T.FlagDefault(1),
+                    T.FlagForced(1),
+                    T.FlagLacing(0),
+                    T.MinCache(0),
+                    T.MaxBlockAdditionID(0),
+                    T.CodecID('V_MPEG4/ISO/AVC'),
+                    T.CodecPrivate(codecPrivate(frame)),
+                    T.CodecDecodeAll(1),
+                    T.SeekPreRoll(0),
+                    T.Video([
+                        T.FlagInterlaced(0),
+                        T.PixelWidth(1280),
+                        T.PixelHeight(720),
+                    ])
+                ])
+            ])
+        ], true)
+    ]);
 };
 
 function codecPrivate(frame) {
-    var list = [];
+    var parts = [];
     var b;
 
     var sps = frame.nals.filter(function(nal) {
@@ -172,57 +119,50 @@ function codecPrivate(frame) {
     b.writeUInt8(sps[0].buf[7], 3); // level
     b.writeUInt8(0xFF, 4); // 6: reserved, 2: Size of NALU lengths, minus 1
     b.writeUInt8(0xE0 | sps.length, 5); // 3: reserved, 5: num SPS
-    list.push(b);
+    parts.push(b);
 
     sps.forEach(function(nal) {
         b = new Buffer(2);
         b.writeUInt16BE(nal.buf.length - 4, 0);
-        list.push(b);
-        list.push(nal.buf.slice(4));
+        parts.push(b);
+        parts.push(nal.buf.slice(4));
     });
 
     b = new Buffer(1);
     b.writeUInt8(pps.length, 0);
-    list.push(b);
+    parts.push(b);
 
     pps.forEach(function(nal) {
         b = new Buffer(2);
         b.writeUInt16BE(nal.buf.length - 4, 0);
-        list.push(b);
-        list.push(nal.buf.slice(4));
+        parts.push(b);
+        parts.push(nal.buf.slice(4));
     });
 
-    return Buffer.concat(list);
+    return Buffer.concat(parts);
 }
 
 exports.frame = function(frame) {
-    var list = [];
-    var b, l, ll, lll;
+    var b;
+    var block = [];
 
-    l = []; // Cluster
-    writeUInt64(ID_Timecode, frame.dts, l);
+    b = new Buffer(4);
+    b.writeUInt8(1 | 0x80, 0);  // Track number as varint
+    b.writeInt16BE(0, 1);  // Timecode
+    b.writeUInt8(frame.keyframe ? 0x01 : 0x00, 3);  // Flags
+    block.push(b);
 
-    ll = []; // SimpleBlock
-
-    b = varInt(1, 1); // Track number
-    ll.push(b);
-
-    b = new Buffer(3);
-    b.writeInt16BE(0, 0); // Timecode
-    b.writeUInt8(frame.keyframe ? 0x01 : 0x00, 2); // Flags
-    ll.push(b);
-
-    // Data
     frame.nals.forEach(function(nal) {
         b = new Buffer(4);
         b.writeUInt32BE(nal.buf.length - 4, 0);
-        ll.push(b);
-        ll.push(nal.buf.slice(4));
+        block.push(b);
+        block.push(nal.buf.slice(4));
     });
 
-    writeBinary(ID_SimpleBlock, Buffer.concat(ll), l);
-
-    writeContainer(ID_Cluster, l, list);
-
-    return Buffer.concat(list);
+    return native.buildEBML([
+        T.Cluster([
+            T.Timecode(frame.dts),
+            T.SimpleBlock(Buffer.concat(block))
+        ])
+    ]);
 };
--- api/native/module.cc
+++ api/native/module.cc
@@ -7,39 +7,54 @@ namespace p1stream {
 using namespace v8;
 using namespace node;
 
+static size_t calc_varint_size(uint64_t val);
+static size_t write_varint(uint64_t val, uint8_t *p);
+static Handle<Value> calc_ebml_content_size(char type, Handle<Value> contentVal, size_t &size);
+static Handle<Value> calc_ebml_size(Handle<Value> val, size_t &size);
+static Handle<Value> write_ebml(Handle<Value> tags, uint8_t *dst);
+static Handle<Value> build_ebml(const Arguments &args);
 
 static Persistent<Function> fast_buffer_constructor;
 
 
-static size_t var_int(uint8_t *p, int64_t val, uint32_t maxlen)
+static size_t calc_varint_size(uint64_t val)
 {
-    if (val < 0) {
-        p[0] = 0xFF;
-        return 1;
-    }
-    else if (val < 0x7F) {
+    if (val < 0x7F) return 1;
+    else if (val < 0x3FFF) return 2;
+    else if (val < 0x1FFFFF) return 3;
+    else if (val < 0x0FFFFFFF) return 4;
+    else if (val < 0x07FFFFFFFF) return 5;
+    else if (val < 0x03FFFFFFFFFF) return 6;
+    else if (val < 0x01FFFFFFFFFFFF) return 7;
+    else return 8;
+}
+
+
+static size_t write_varint(uint64_t val, uint8_t *p)
+{
+    if (val < 0x7F) {
         p[0] = val | 0x80;
         return 1;
     }
-    else if (maxlen >= 2 && val < 0x3FFF) {
+    else if (val < 0x3FFF) {
         p[0] = ((val & 0xFF00) >> 8) | 0x40;
         p[1] =  (val & 0x00FF);
         return 2;
     }
-    else if (maxlen >= 3 && val < 0x1FFFFF) {
+    else if (val < 0x1FFFFF) {
         p[0] = ((val & 0xFF0000) >> 16) | 0x20;
         p[1] =  (val & 0x00FF00) >> 8;
         p[2] =  (val & 0x0000FF);
         return 3;
     }
-    else if (maxlen >= 4 && val < 0x0FFFFFFF) {
+    else if (val < 0x0FFFFFFF) {
         p[0] = ((val & 0xFF000000) >> 24) | 0x10;
         p[1] =  (val & 0x00FF0000) >> 16;
         p[2] =  (val & 0x0000FF00) >> 8;
         p[3] =  (val & 0x000000FF);
         return 4;
     }
-    else if (maxlen >= 5 && val < 0x07FFFFFFFF) {
+    else if (val < 0x07FFFFFFFF) {
         p[0] = ((val & 0xFF00000000) >> 32) | 0x08;
         p[1] =  (val & 0x00FF000000) >> 24;
         p[2] =  (val & 0x0000FF0000) >> 16;
@@ -47,7 +62,7 @@ static size_t var_int(uint8_t *p, int64_t val, uint32_t maxlen)
         p[4] =  (val & 0x00000000FF);
         return 5;
     }
-    else if (maxlen >= 6 && val < 0x03FFFFFFFFFF) {
+    else if (val < 0x03FFFFFFFFFF) {
         p[0] = ((val & 0xFF0000000000) >> 40) | 0x04;
         p[1] =  (val & 0x00FF00000000) >> 32;
         p[2] =  (val & 0x0000FF000000) >> 24;
@@ -56,7 +71,7 @@ static size_t var_int(uint8_t *p, int64_t val, uint32_t maxlen)
         p[5] =  (val & 0x0000000000FF);
         return 6;
     }
-    else if (maxlen >= 7 && val < 0x01FFFFFFFFFFFF) {
+    else if (val < 0x01FFFFFFFFFFFF) {
         p[0] = ((val & 0xFF000000000000) >> 48) | 0x02;
         p[1] =  (val & 0x00FF0000000000) >> 40;
         p[2] =  (val & 0x0000FF00000000) >> 32;
@@ -66,7 +81,7 @@ static size_t var_int(uint8_t *p, int64_t val, uint32_t maxlen)
         p[6] =  (val & 0x000000000000FF);
         return 7;
     }
-    else if (maxlen == 8 && val < 0x00FFFFFFFFFFFFFF) {
+    else {
         p[0] = 0x01;
         p[1] =  (val & 0xFF000000000000) >> 48;
         p[2] =  (val & 0x00FF0000000000) >> 40;
@@ -77,34 +92,207 @@ static size_t var_int(uint8_t *p, int64_t val, uint32_t maxlen)
         p[7] =  (val & 0x000000000000FF);
         return 8;
     }
-    return 0;
 }
 
 
-static Handle<Value> var_int(const Arguments &args)
+static Handle<Value> calc_ebml_content_size(char type, Handle<Value> contentVal, size_t &size)
 {
-    if (args.Length() != 2 || !args[0]->IsNumber() || !args[1]->IsUint32())
+    switch (type) {
+        case 'm': {
+            size = 0;
+            return calc_ebml_size(contentVal, size);
+        }
+        case 'u': {
+            double number = contentVal->NumberValue();
+            if (number < 0)
+                return ThrowException(Exception::TypeError(
+                            String::New("Invalid unsigned value")));
+            else if (number <= 0xFF) size = 1;
+            else if (number <= 0xFFFF) size = 2;
+            else if (number <= 0xFFFFFFFF) size = 4;
+            else if (number <= 0xFFFFFFFFFFFFFFFF) size = 8;
+            else
+                return ThrowException(Exception::TypeError(
+                            String::New("Invalid unsigned value")));
+            break;
+        }
+        case 's':
+        case '8': {
+            size = contentVal->ToString()->Utf8Length();
+            break;
+        }
+        case 'b': {
+            if (!Buffer::HasInstance(contentVal))
+                return ThrowException(Exception::TypeError(
+                            String::New("Invalid buffer value")));
+            size = Buffer::Length(contentVal);
+            break;
+        }
+        default:
+            return ThrowException(Exception::TypeError(
+                        String::New("Invalid tag type")));
+    }
+
+    return Handle<Value>();
+}
+
+
+static Handle<Value> calc_ebml_size(Handle<Value> val, size_t &size)
+{
+    if (!val->IsArray())
         return ThrowException(Exception::TypeError(
-                    String::New("Invalid arguments")));
+                    String::New("Expected an array of tags")));
 
-    double val = args[0]->NumberValue();
-    uint32_t maxlen = args[1]->Uint32Value();
-    if (maxlen < 1 || maxlen > 8)
-        return ThrowException(Exception::Error(
-                    String::New("Invalid maxlen")));
+    auto tags = Handle<Array>::Cast(val);
+    uint32_t length = tags->Length();
+    for (uint32_t i = 0; i < length; i++) {
+        auto tagVal = tags->Get(i);
+        if (!tagVal->IsArray())
+            return ThrowException(Exception::TypeError(
+                        String::New("Invalid tag")));
 
-    uint8_t buf[maxlen];
-    size_t len = var_int(buf, val, maxlen);
-    if (len == 0)
-        return ThrowException(Exception::Error(
-                    String::New("Could not encode varint")));
+        auto tag = Handle<Array>::Cast(tagVal);
+        auto tagLength = tag->Length();
+        if (tagLength < 3 || tagLength > 4)
+            return ThrowException(Exception::TypeError(
+                        String::New("Invalid tag")));
 
-    Handle<Value> arg = Number::New(len);
+        auto idVal = tag->Get(0);
+        auto typeVal = tag->Get(1);
+        auto contentVal = tag->Get(2);
+
+        if (!Buffer::HasInstance(idVal))
+            return ThrowException(Exception::TypeError(
+                        String::New("Invalid tag ID")));
+
+        auto idSize = Buffer::Length(idVal);
+        if (idSize < 1 || idSize > 4)
+            return ThrowException(Exception::TypeError(
+                        String::New("Invalid tag ID")));
+
+        String::AsciiValue typeStr(typeVal);
+        if (typeStr.length() != 1)
+            return ThrowException(Exception::TypeError(
+                        String::New("Invalid tag type")));
+
+        size_t contentSize;
+        auto ret = calc_ebml_content_size(**typeStr, contentVal, contentSize);
+        if (!ret.IsEmpty())
+            return ret;
+
+        size_t sizeSize = tag->Get(3)->IsTrue() ? 1 : calc_varint_size(contentSize);
+
+        size += idSize + sizeSize + contentSize;
+    }
+
+    return Handle<Value>();
+}
+
+static Handle<Value> write_ebml(Handle<Value> val, uint8_t *dst)
+{
+    auto tags = Handle<Array>::Cast(val);
+    uint32_t length = tags->Length();
+    for (uint32_t i = 0; i < length; i++) {
+        auto tag = Handle<Array>::Cast(tags->Get(i));
+
+        auto idVal = tag->Get(0);
+        auto typeVal = tag->Get(1);
+        auto contentVal = tag->Get(2);
+
+        auto idSize = Buffer::Length(idVal);
+        memcpy(dst, Buffer::Data(idVal), idSize);
+        dst += idSize;
+
+        size_t contentSize;
+        String::AsciiValue typeStr(typeVal);
+        auto ret = calc_ebml_content_size(**typeStr, contentVal, contentSize);
+        if (!ret.IsEmpty())
+            return ret;
+
+        if (tag->Get(3)->IsTrue()) {
+            *dst = 0xFF;
+            dst += 1;
+        }
+        else {
+            dst += write_varint(contentSize, dst);
+        }
+
+        switch (**typeStr) {
+            case 'm': {
+                auto ret = write_ebml(contentVal, dst);
+                if (!ret.IsEmpty())
+                    return ret;
+                break;
+            }
+            case 'u': {
+                uint64_t number = contentVal->NumberValue();
+                if (number <= 0xFF) {
+                    dst[0] = number;
+                }
+                else if (number <= 0xFFFF) {
+                    dst[0] = (number & 0xFF00) >> 8;
+                    dst[1] = (number & 0x00FF);
+                }
+                else if (number <= 0xFFFFFFFF) {
+                    dst[0] = (number & 0xFF000000) >> 24;
+                    dst[1] = (number & 0x00FF0000) >> 16;
+                    dst[2] = (number & 0x0000FF00) >> 8;
+                    dst[3] = (number & 0x000000FF);
+                }
+                else if (number <= 0xFFFFFFFFFFFFFFFF) {
+                    dst[0] = (number & 0xFF00000000000000) >> 56;
+                    dst[1] = (number & 0x00FF000000000000) >> 48;
+                    dst[2] = (number & 0x0000FF0000000000) >> 40;
+                    dst[3] = (number & 0x000000FF00000000) >> 32;
+                    dst[4] = (number & 0x00000000FF000000) >> 24;
+                    dst[5] = (number & 0x0000000000FF0000) >> 16;
+                    dst[6] = (number & 0x000000000000FF00) >> 8;
+                    dst[7] = (number & 0x00000000000000FF);
+                }
+                break;
+            }
+            case 's':
+            case '8': {
+                contentVal->ToString()->WriteUtf8((char *) dst, -1, NULL,
+                        String::NO_NULL_TERMINATION | String::PRESERVE_ASCII_NULL);
+                break;
+            }
+            case 'b': {
+                memcpy(dst, Buffer::Data(contentVal), contentSize);
+                break;
+            }
+        }
+
+        dst += contentSize;
+    }
+
+    return Handle<Value>();
+}
+
+
+static Handle<Value> build_ebml(const Arguments &args)
+{
+    HandleScope scope;
+    Handle<Value> ret;
+
+    if (args.Length() != 1)
+        return ThrowException(Exception::TypeError(
+                    String::New("Expected one argument")));
+
+    size_t size = 0;
+    ret = calc_ebml_size(args[0], size);
+    if (!ret.IsEmpty())
+        return ret;
+
+    Handle<Value> arg = Number::New(size);
     auto bufobj = fast_buffer_constructor->NewInstance(1, &arg);
-    auto dst = Buffer::Data(bufobj);
-    memcpy(dst, buf, len);
+    uint8_t *dst = (uint8_t *) Buffer::Data(bufobj);
 
-    return bufobj;
+    ret = write_ebml(args[0], dst);
+    if (!ret.IsEmpty())
+        return ret;
+
+    return scope.Close(bufobj);
 }
 
 
@@ -117,8 +305,8 @@ static void init(Handle<Object> e)
     auto fn = Handle<Function>::Cast(val);
     fast_buffer_constructor = Persistent<Function>::New(fn);
 
-    func = FunctionTemplate::New(var_int);
-    e->Set(String::NewSymbol("varInt"), func->GetFunction());
+    func = FunctionTemplate::New(build_ebml);
+    e->Set(String::NewSymbol("buildEBML"), func->GetFunction());
 }
 
 
