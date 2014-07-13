6dd7b6573205d7ed2aa8f001ba6869d739013f4f
Stéphan Kochen <stephan@kochen.nl>
2014-07-13 14:08:40 +0200

    Change staticFiles to be lazy.
    Together with the previous commit, we can now develop the webui without
    restarting.

--- api/lib/staticFiles.js
+++ api/lib/staticFiles.js
@@ -11,61 +11,47 @@ var mimeTypes = {
 // `base` is the base request path.
 // `docroot` is the file system path.
 module.exports = function(base, docroot) {
-    var map = Object.create(null);
-    if (base.slice(-1) === '/')
-        base = base.slice(0, -1);
-
-    if (base)
-        map[base] = { redir: base + '/' };
-    else
-        base = '/';
-
-    function walk(subdir) {
-        var dir = path.join(docroot, subdir);
-        fs.readdirSync(dir).forEach(function(entry) {
-            var from = path.join(base, subdir, entry);
-            var to = path.join(dir, entry);
-            var stat = fs.statSync(to);
-            if (stat.isFile()) {
-                var f = map[from] = {
-                    file: to,
-                    size: stat.size,
-                    mime: mimeTypes[path.extname(to)] ||
-                          'application/octet-stream'
-                };
-                if (entry === 'index.html')
-                    map[from.slice(0, -10)] = f;
-            }
-            else if (stat.isDirectory()) {
-                map[from] = { redir: from + '/' };
-                walk(path.join(subdir, entry));
-            }
-        });
+    if (typeof(base) === 'string') {
+        if (base.slice(-1) === '/')
+            base = base.slice(0, -1);
+        base = new RegExp('^' + base);
     }
-    walk('');
 
     return function(req, res, next) {
-        if (req.method === 'GET') {
-            var f = map[req.parsed.pathname];
-            if (!f)
-                return next();
+        var p = req.parsed.pathname;
+        if (req.method !== 'GET' || !base.test(p))
+            return next();
 
-            if (f.redir) {
+        p = path.resolve('/', decodeURI(p));
+        if (p.slice(-1) === '/')
+            p += 'index.html';
+
+        var fp = path.join(docroot, p);
+        fs.stat(fp, function(err, stats) {
+            if (err)
+                return next(err.code === 'ENOENT' ? null : err);
+
+            if (stats.isDirectory()) {
                 res.writeHead(301, {
-                    'Location': f.redir,
-                    'Content-Type': 'text/plain',
+                    'Location': p + '/',
                     'Content-Length': 0
                 });
                 res.end();
                 return;
             }
 
-            var stream = fs.createReadStream(f.file);
-            res.writeHead(200, {
-                'Content-Type': f.mime,
-                'Content-Length': f.size
+            var stream = fs.createReadStream(fp);
+            stream.on('error', next);
+            stream.on('open', function(fd) {
+                var ext = path.extname(p);
+                var mime = mimeTypes.hasOwnProperty(ext) ?
+                    mimeTypes[ext] : 'application/octet-stream';
+                res.writeHead(200, {
+                    'Content-Type': mime,
+                    'Content-Length': stats.size
+                });
+                stream.pipe(res);
             });
-            stream.pipe(res);
-        }
+        });
     };
 };
