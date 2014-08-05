6e7a776f3f66abb13890665d56f191db5f575121
Stéphan Kochen <stephan@kochen.nl>
2014-08-05 19:30:07 +0200

    Wrap stream creation in an api call, untested.
    Mostly just so I can do some webdev on this machine that can't handle
    the video mixer magic.

--- api/index.js
+++ api/index.js
@@ -6,43 +6,54 @@ var core = require('../core');
 module.exports = function(app) {
     app.cfg = config();
 
-    var video = new core.Video();
-    var audio = new core.Audio();
-    var mstream = new events.EventEmitter();
+    var streams = Object.create(null);
 
-    var videoHeaders = null;
-    var audioHeaders = null;
-    video.once('headers', function(frame) {
-        videoHeaders = frame;
-        buildHeaders();
+    app.post('/api/streams/new/:id', function(req, res, next) {
+        var video, audio, mstream;
+        var stream = streams[req.params.id] = {
+            video: video = new core.Video(),
+            audio: audio = new core.Audio(),
+            mstream: mstream = new events.EventEmitter(),
+            headers: null
+        };
+
+        var videoHeaders = null;
+        var audioHeaders = null;
+        video.once('headers', function(frame) {
+            videoHeaders = frame;
+            buildHeaders();
+        });
+        audio.once('headers', function(frame) {
+            audioHeaders = frame;
+            buildHeaders();
+        });
+
+        function buildHeaders() {
+            if (videoHeaders && audioHeaders)
+                stream.headers = matroska.headers(videoHeaders, audioHeaders);
+        }
+
+        video.on('frame', function(frame) {
+            mstream.emit('frame', matroska.videoFrame(frame), frame.keyframe);
+        });
+
+        audio.on('frame', function(frame) {
+            mstream.emit('frame', matroska.audioFrame(frame));
+        });
     });
-    audio.once('headers', function(frame) {
-        audioHeaders = frame;
-        buildHeaders();
-    });
 
-    var headers = null;
-    function buildHeaders() {
-        if (videoHeaders && audioHeaders)
-            headers = matroska.headers(videoHeaders, audioHeaders);
-    }
+    app.get('/api/streams/:id.mkv', function(req, res, next) {
+        var stream = streams[req.params.id];
+        if (!stream)
+            return res.send(404);
 
-    video.on('frame', function(frame) {
-        mstream.emit('frame', matroska.videoFrame(frame), frame.keyframe);
-    });
-
-    audio.on('frame', function(frame) {
-        mstream.emit('frame', matroska.audioFrame(frame));
-    });
-
-    app.get('/api/stream.mkv', function(req, res, next) {
-        mstream.addListener('frame', onFrame);
+        stream.mstream.addListener('frame', onFrame);
         res.on('close', function() {
-            mstream.removeListener('frame', onFrame);
+            stream.mstream.removeListener('frame', onFrame);
         });
 
         function onFrame(frame, keyframe) {
-            if (!headers) return;
+            if (!stream.headers) return;
 
             if (!res.headersSent) {
                 if (!keyframe) return;
@@ -52,7 +63,7 @@ module.exports = function(app) {
                     'Connection': 'close',
                     'Content-Type': 'video/x-matroska'
                 });
-                res.write(headers);
+                res.write(stream.headers);
             }
 
             res.write(frame);
