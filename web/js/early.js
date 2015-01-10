384b65bbe8815ece7d1b0807eb703fe7714b5663
Stéphan Kochen <stephan@kochen.nl>
2015-01-10 16:36:59 +0100

    Minor changes.

--- web/js/early.js
+++ web/js/early.js
@@ -1,4 +1,7 @@
 // Destroy the atom-shell commonjs environment.
 // This stops scripts from detecting it and changing behavior.
-if (typeof(module) !== 'undefined')
+if (typeof module !== 'undefined') {
+    // jshint -W020
     require = exports = module = undefined;
+    // jshint +W020
+}
