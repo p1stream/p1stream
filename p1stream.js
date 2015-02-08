1c507a1b8ba298c3d09997ef6eced8c179446f07
Stéphan Kochen <stephan@kochen.nl>
2015-02-08 10:18:23 +0100

    Start of an RPC mode.

--- p1stream.js
+++ p1stream.js
@@ -1,10 +1,30 @@
 #!/usr/bin/env iojs
 
 var p1stream = require('./');
+var urllib = require('url');
 var port = process.env.PORT || 53311;
+
+var rpc;
+if (process.argv[2] === '--rpc') {
+    rpc = require('jmsg').streams(process.stdin, process.stdout);
+    process.stdin.on('end', function() {
+        process.stderr.write("STDIN closed, stopping.\n", function() {
+            process.exit(0);
+        });
+    });
+}
 
 var scope = p1stream();
 scope.server.listen(port, '127.0.0.1', function() {
     var addr = scope.server.address();
-    console.log('Listening on http://' + addr.address + ':' + addr.port + '/');
+    var url = urllib.format({
+        protocol: 'http',
+        hostname: addr.address,
+        port: addr.port,
+        pathname: '/'
+    });
+    if (rpc)
+        rpc.send('started', { url: url });
+    else
+        console.log('Listening on ' + url);
 });
