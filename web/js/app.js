bad80c09c6a7ee4f259f15bc0a41ca8f3602fb10
Stéphan Kochen <stephan@kochen.nl>
2015-02-24 19:33:04 +0100

    Set null prototype on sync objects in browser.

--- web/js/app.js
+++ web/js/app.js
@@ -2,26 +2,39 @@
 
 var module = angular.module('p1stream', []);
 
-module.run(['$rootScope',
-function($rootScope) {
+function nullReviver(k, v) {
+    if (typeof(v) === 'object' && v !== null)
+        return angular.extend(Object.create(null), v);
+    else
+        return v;
+}
+
+module.run(function($rootScope) {
     var symmetryOptions = { preserve: true };
 
-    var objects = $rootScope.objects = Object.create(null);
+    var objects = $rootScope.o = Object.create(null);
     var source = new EventSource('/api/objects.sse');
 
     source.addEventListener('reset', function(ev) {
         $rootScope.$apply(function() {
-            var val = JSON.parse(ev.data);
+            var val = JSON.parse(ev.data, nullReviver);
             Symmetry.resetObject(objects, val, symmetryOptions);
+            setIds();
         });
     });
 
     source.addEventListener('patch', function(ev) {
         $rootScope.$apply(function() {
-            var patch = JSON.parse(ev.data);
+            var patch = JSON.parse(ev.data, nullReviver);
             Symmetry.patch(objects, patch, symmetryOptions);
+            setIds();
         });
     });
-}]);
+
+    function setIds() {
+        for (var id in objects)
+            objects[id].$id = id;
+    }
+});
 
 })();
