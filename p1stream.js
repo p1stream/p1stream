70ced7ef7208a35dacab4e07416dcd385c323e79
Stéphan Kochen <stephan@kochen.nl>
2015-03-04 20:07:27 +0100

    Bunyan logging to stdout (console / rpc).

--- p1stream.js
+++ p1stream.js
@@ -4,17 +4,18 @@ var p1stream = require('./');
 var urllib = require('url');
 var port = process.env.PORT || 53311;
 
-var rpc;
+var scope = p1stream();
+
 if (process.argv[2] === '--rpc') {
-    rpc = require('jmsg').streams(process.stdin, process.stdout);
+    scope.rpc = require('jmsg').streams(process.stdin, process.stdout);
     process.stdin.on('end', function() {
-        process.stderr.write("STDIN closed, stopping.\n", function() {
+        scope.log.info("STDIN closed in RPC mode, stopping.");
+        setTimeout(function() {
             process.exit(0);
-        });
+        }, 1000);
     });
 }
 
-var scope = p1stream();
 scope.server.listen(port, '127.0.0.1', function() {
     var addr = scope.server.address();
     var url = urllib.format({
@@ -23,8 +24,7 @@ scope.server.listen(port, '127.0.0.1', function() {
         port: addr.port,
         pathname: '/'
     });
-    if (rpc)
-        rpc.send('started', { url: url });
-    else
-        console.log('Listening on ' + url);
+    scope.log.info('Listening on %s', url);
+    if (scope.rpc)
+        scope.rpc.send('started', { url: url });
 });
